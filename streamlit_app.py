b318200bc6c0f6863dcc9712d6bce267c980d579
Akash maurya <44486527+akashmaurya517@users.noreply.github.com>
2022-06-05 13:14:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,9 @@ if __name__ == "__main__":
         #components.iframe("https://docs.streamlit.io/en/latest")
         col1, col2, col3, col4 = st.columns(4)
         with col1:
-            st.text("Data Science Professional Certificate")
+            st.text("""Data Science
+            Professional
+            Certificate""")
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="c71eb334-9ced-4463-955d-dded93f6f364" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col2:
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="838f5fcb-b703-46e6-aecc-dec1942a2b58" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
